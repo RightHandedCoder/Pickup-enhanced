220095470ef6439bb844489061ff45f70c932200
Shams Bin Sohrab <aniknishan@gmail.com>
2018-01-02 20:45:51 +0600

    going on...

---
+++ Pickup/ApiScripts/DeleteProduct.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    function GetId() {
+        var result = window.location.pathname.split('/Product/Edit/');
+
+        return result[1];
+    }
+    var id = GetId();
+    console.log(id);
+
+    $('#btnDelete').click(function () {
+
+        $.ajax({
+
+            url: 'http://localhost:52683/api/Home/'+id,
+            method: 'DELETE',
+            complete: function (xhr)
+            {
+                alert(xhr.statusText);
+            }
+        })
+    })
+})
