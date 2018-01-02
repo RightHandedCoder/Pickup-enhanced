5aa002d6756f8705107d8d5985565b10c3203bca
Shams Bin Sohrab <aniknishan@gmail.com>
2018-01-02 20:06:53 +0600

    Working on Admin

--- Pickup/Controllers/AdminController.cs
+++ Pickup/Controllers/AdminController.cs
@@ -11,17 +11,21 @@ namespace Pickup.Controllers
 {
     public class AdminController : Controller
     {
-        IAdminService service;
+        IAdminService adminService;
+        ICatagoryService catagoryService;
+        IAreaService areaService;
        
 
         public AdminController()
         {
-            service = Injector.Container.Resolve<IAdminService>(); 
+            adminService = Injector.Container.Resolve<IAdminService>();
+            catagoryService = Injector.Container.Resolve<ICatagoryService>();
+            areaService = Injector.Container.Resolve<IAreaService>();
         }
 
         public ActionResult Index(int id)
         {
-            Admin admin = service.Get(id);
+            Admin admin = adminService.Get(id);
 
             return View(admin);
         }
@@ -34,6 +38,58 @@ namespace Pickup.Controllers
         public ActionResult EditProduct(int id)
         {
             return RedirectToAction("Edit", "Product", new { @id = id });
+        }
+
+        public ActionResult AddArea()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddArea(Area a)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    areaService.Insert(a);
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (Exception)
+                {
+
+                    return View("Error");
+                }
+            }
+
+            return View(a);
+        }
+
+        public ActionResult AddCatagory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCatagory(Catagory c)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    catagoryService.Insert(c);
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (Exception)
+                {
+
+                    return View("Error");
+                }
+
+                
+            }
+
+            return View(c);
         }
 
 
