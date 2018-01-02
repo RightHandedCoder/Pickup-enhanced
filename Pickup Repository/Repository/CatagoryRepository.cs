315feaea3bf422fc25d7ebe955e75d095cf348ae
Md.Nahid Nawaz <nahid.nawaz96@gmail.com>
2018-01-02 20:04:22 +0600

    Repository Update Created

--- Pickup Repository/Repository/CatagoryRepository.cs
+++ Pickup Repository/Repository/CatagoryRepository.cs
@@ -9,5 +9,13 @@ namespace Pickup_Repository
 {
     public class CatagoryRepository : Repository<Catagory>, ICatagoryRepository
     {
+        DataContext context = new DataContext();
+        public override int Update(Catagory entity)
+        {
+            Catagory c = context.Catagories.FirstOrDefault(a => a.Id == entity.Id);
+
+            c.CatagoryName = entity.CatagoryName;
+            return context.SaveChanges();
+        }
     }
 }
--- Pickup Repository/Repository/DepartmentRepository.cs
+++ Pickup Repository/Repository/DepartmentRepository.cs
@@ -9,5 +9,16 @@ namespace Pickup_Repository
 {
     public class DepartmentRepository : Repository<Department>, IDepartmentRepository
     {
+        DataContext context = new DataContext();
+
+        public override int Update(Department entity)
+        {
+            Department d = context.Departments.FirstOrDefault(a => a.Id == entity.Id);
+
+            d.DepartmentName = entity.DepartmentName;
+            d.DepartmentLocation = entity.DepartmentLocation;
+
+            return context.SaveChanges();
+        }
     }
 }
